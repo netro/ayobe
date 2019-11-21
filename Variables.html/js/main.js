aa0c7ee492156fbfda7b2df5886fdd088028e89f
MOHAMAD1999999 <58008008+MOHAMAD1999999@users.noreply.github.com>
2019-11-21 13:08:30 +0300

    Variables

---
+++ Variables.html/js/main.js
@@ -0,0 +1,19 @@
+
+
+var mainPrice = 500,
+
+   mySmallDiscount = 50,
+
+   myMediumDiscount = 100,
+
+   myBigDiscount = 250;
+
+   document.getElementById("price").innerHTML = mainPrice;
+
+document.getElementById("price").innerHTML = mainPrice - mySmallDiscount;
+
+document.getElementById("price").innerHTML = mainPrice - myMediumDiscount;
+
+document.getElementById("price").innerHTML = mainPrice - myBigDiscount;
+
+
--- Variables.html/main.js
+++
@@ -1,4 +0,0 @@
-var myOldprice = 2000;
-var myNewprice = 900;
-var myDiscount = myOldprice - myNewprice;
-document.getElementById("price").innerHTML = myDiscount;

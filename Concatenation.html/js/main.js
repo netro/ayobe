2d71d01b9c55fa352c28e1eedbd8a0f55bf0c3b9
MOHAMAD1999999 <58008008+MOHAMAD1999999@users.noreply.github.com>
2019-11-22 17:24:52 +0300

    Concatenation

---
+++ Concatenation.html/js/main.js
@@ -0,0 +1,10 @@
+var myName = "Osama",
+
+myAge = 32,
+
+myCountry = "syria";
+
+document.getElementById("test").innerHTML = 
+"my Name Is: " + myName + "<br>" +
+"myAge Is:" + myAge + "<br>" +
+ "myCountry Is: " + myCountry;

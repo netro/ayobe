8e44e2a9476e8748839567d02f4df83e4b1e55d7
MOHAMAD1999999 <58008008+MOHAMAD1999999@users.noreply.github.com>
2019-11-29 16:08:25 +0300

    Conditional Operators

---
+++ Conditional Operators/js/main.js
@@ -0,0 +1,15 @@
+/*
+(=) Assignment Operator 
+(==) Copmarison Operator Compare Value
+(===) Identity Operator Copmpare Data Type + Value 
+*/
+
+var osama = "50"// Data Type <= String 
+
+if (osama == 50){ // Data Type <= Number
+alert("Good");
+
+ } else {
+     
+    alert("Bad");
+}

b691eb3d28a0aa8e5ccd7914d073f6792b6f7661
MOHAMAD1999999 <58008008+MOHAMAD1999999@users.noreply.github.com>
2019-12-04 21:07:07 +0300

    Function - Basic

---
+++ Function - Basic/js/main.js
@@ -0,0 +1,14 @@
+function sayHi(){
+
+    "use strict"
+
+    var myName = "Osama";
+
+    if(myName === "Osama") {
+
+        alert("Hell0 " + myName + "You Are Weclome");
+    } else {
+         alert ("Hello " + myName + "You Are Not Welcome");
+    }
+}
+sayHi();

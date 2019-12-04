913599f501b849120148c369adf566b4d0f0e585
MOHAMAD1999999 <58008008+MOHAMAD1999999@users.noreply.github.com>
2019-12-04 20:30:23 +0300

    Logical Operators 2

---
+++ Logical Operators 2/js/main.js
@@ -0,0 +1,45 @@
+/*
+(!) Not Equal
+
+(!=) ! Not = Equal (Not Equal )
+
+(!==) Not Identical 
+
+(&&)
+
+*/
+ var 
+
+ name = "Osama",
+
+ Age = "32",
+
+ address = "KSA";
+
+ if (age == "32" && 
+    name == "Osama" && 
+    address == "KSA"); {
+
+     alert("Good");
+
+ } else {
+     alert("Bad");
+ }
+
+var
+
+    name = "Osama",
+
+    Age = "32",
+
+    address = "KSA";
+
+if (age == "32" ||
+    name == "Osama" ||
+    address == "KSA"); {
+
+    alert("Good");
+
+} else {
+    alert("Bad");
+}
